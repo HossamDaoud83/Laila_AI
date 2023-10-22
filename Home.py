ef00c540413ce0dd2b78e9eee9a196c89c54ef25
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-10-22 07:10:35 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,8 +21,8 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # Create a button that links to an external website
 external_link = "https://poe.com/LailaGSB/"  # Replace with the actual URL
 if st.button("Chat with Laila AI"):
-    webbrowser.open_new_tab(external_link)        
-    #st.markdown(f"Redirecting to [{external_link}]({external_link})")
+    #webbrowser.open_new_tab(external_link)        
+    st.markdown(f"Redirecting to [{external_link}]({external_link})")
     
 
 hide_st_style = """
